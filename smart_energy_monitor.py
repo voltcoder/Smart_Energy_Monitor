34f67b290c1f906c53f461098a92973f6508f101
Swati Sharma <swatisharma83915@gmail.com>
2026-03-20 22:22:26 +0530

    Update smart_energy_monitor.py

--- smart_energy_monitor.py
+++ smart_energy_monitor.py
@@ -1,1 +1,16 @@
+import random
+import time
 
+print("---- Smart Energy Monitor ----")
+
+voltage = 230   # constant supply
+
+for i in range(10):  # simulate readings
+    current = random.uniform(0.5, 5)   # random current
+    power = voltage * current          # P = V × I
+
+    print(f"Voltage: {voltage} V")
+    print(f"Current: {current:.2f} A")
+    print(f"Power: {power:.2f} W\n")
+
+    time.sleep(1)
