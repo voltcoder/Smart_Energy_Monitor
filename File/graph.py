467631a2ec03ee23aced3c6356f3a0040c2e5053
Swati Sharma <swatisharma83915@gmail.com>
2026-03-20 22:20:09 +0530

    Update graph.py

--- File/graph.py
+++ File/graph.py
@@ -1,1 +1,15 @@
+import matplotlib.pyplot as plt
+import random
 
+power_data = []
+
+for i in range(20):
+    current = random.uniform(0.5, 5)
+    power = 230 * current
+    power_data.append(power)
+
+plt.plot(power_data)
+plt.title("Power Consumption Trend")
+plt.xlabel("Time")
+plt.ylabel("Power (W)")
+plt.show()
